ea7e2f6929f2241cae64daabba8f786d04d58957
Ashleyjeffers2002 <43543974+Ashleyjeffers2002@users.noreply.github.com>
2019-03-20 10:17:34 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#name").click(function(){
+  $(".margo").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("#crew").click(function(){
+ $("#fire").slideToggle();
+});
+
+
+
+//Spell #3
+
+$(".button3").click(function(){
+    $("#Evil").toggle();
+});
+
+
+
